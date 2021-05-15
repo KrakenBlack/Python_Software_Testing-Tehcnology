10a8ce18a51eacf22d0fe8462adfe9d4ba1fded7
Silver <silver.luhtoja@gmail.com>
2021-05-15 11:20:41 +0300

    testing Contact

--- contact.py
+++ contact.py
@@ -1,3 +1,10 @@
 
 class Contact:
-    pass
+
+
+    def __init__(self, name,home_phone = None, mob_phone = None):
+        self.name = name
+
+        if home_phone is None and mob_phone is None:
+            raise ValueError
+
--- test_contact.py
+++ test_contact.py
@@ -3,11 +3,45 @@ import pytest
 
 @pytest.fixture()
 def valid_name():
-    return 'Connect'
+    return 'Connect'\
+
+@pytest.fixture()
+def valid_phone():
+    return ''
+
+@pytest.fixture()
+def valid_mob():
+    return ''
+
+
 class TestCreateContact:
+    # def test_create_contact_with_args(self,valid_name,valid_phone,valid_mob):
+    #     # imput
+    #     name = valid_name
+    #     phone = valid_phone
+    #     mob = valid_mob
+    #
+    #
+    #     # process
+    #     contact = Contact(name, phone, mob)
+    #
+    #     #asserting
+    #     assert isinstance(contact,Contact)
+    #     assert contact.name == name
+    #     assert contact.phone == phone
+    #     assert contact.mob == mob
 
-    def test_create_contact(self):
+    def test_without_any_number(self):
+        name = ''
 
-        contact = Contact()
 
-        assert isinstance(contact,Contact)
+        with pytest.raises(ValueError):
+            contact = Contact(name)
+
+    @pytest.mark.skip(reason = 'Not implemented')
+    def test_with_invalid_countrycode(self):
+        pass
+
+    @pytest.mark.skip(reason='Not implemented')
+    def test_with_invalid_email(self):
+        pass
